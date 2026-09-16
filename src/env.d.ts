d9669af8f41b1c652b4a32a1b2c9fdd3c77dc17a
josuejuca <josuejuca19@gmail.com>
2026-09-16 15:17:37 -0300

    feat: adiciona login com Google

--- src/env.d.ts
+++ src/env.d.ts
@@ -11,6 +11,8 @@ declare global {
   interface CloudflareBindings {
     GITHUB_CLIENT_ID: string
     GITHUB_CLIENT_SECRET: string
+    GOOGLE_CLIENT_ID: string
+    GOOGLE_CLIENT_SECRET: string
     SESSION_SECRET: string
   }
 }
